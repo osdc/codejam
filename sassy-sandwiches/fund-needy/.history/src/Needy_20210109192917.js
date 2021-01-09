a2163c9a05643ca6a3eb7e6fe4864f628b311787
Shivansh Gupta <shiv0403gupta@gmail.com>
2021-01-10 01:39:49 +0530

    firebase setup and gitignore

--- sassy-sandwiches/fund-needy/.history/src/Needy_20210109192917.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Needy.css";
-
-function Needy() {
-  return <div className="needy"></div>;
-}
-
-export default Needy;
--- sassy-sandwiches/fund-needy/.history/src/Success_20210109225139.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Success.css";
-
-function Success() {
-  return <div className="success"></div>;
-}
-
-export default Success;
