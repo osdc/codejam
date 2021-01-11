bf81e30f373f9f0c1cd35a50f24bbcbaa44e06cc
Shivansh Gupta <shiv0403gupta@gmail.com>
2021-01-12 02:29:20 +0530

    notification page bugs fix

--- sassy-sandwiches/src/Components/Firm/Notification.js
+++ sassy-sandwiches/src/Components/Firm/Notification.js
@@ -2,23 +2,35 @@ import React, { useState, useEffect } from "react";
 import "./Notification.css";
 import { useAuth } from "../../AuthContext";
 import { auth, db } from "../../firebase";
+import firebase from "firebase";
 
 function Notification() {
   const [people_contributed, setPeopleContributed] = useState([]);
-  const { currentUser } = useAuth();
+  const [copy, setCopy] = useState([]);
+  const [render, setRender] = useState(false);
+  const [cnt, setCnt] = useState(0);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
-    db.collection("users")
-      .doc(currentUser.uid)
-      .get()
-      .then((doc) => {
-        setPeopleContributed(doc.data().people_contributed);
-      });
-  });
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        db.collection("users")
+          .doc(auth.currentUser.uid)
+          .get()
+          .then((doc) => {
+            setPeopleContributed(doc.data().people_contributed.reverse());
+            setCopy(people_contributed);
+          });
+      } else {
+        console.log("No user!");
+      }
+    });
+  }, []);
 
   return (
     <div className="notification">
-      <h1>People who helped you 👍 </h1>
+      <h1>{people_contributed.length} People who helped you 👍 </h1>
+
       {people_contributed.map((person) => {
         return (
           <div className="notification_person">
