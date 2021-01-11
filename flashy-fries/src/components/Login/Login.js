50f6ccc7d603a102f194627308e37b4664da685b
akshat jain <akshat.gms@gmail.com>
2021-01-12 00:58:45 +0530

    ADDED MORE FILES

--- flashy-fries/src/components/Login/Login.js
+++ flashy-fries/src/components/Login/Login.js
@@ -1,5 +1,29 @@
+import "./Login.css";
 const Login = () => {
-  return "login";
+  return (
+    <div className="form">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC1ueC3iCmaW5la1MI4pF2AY2Yp20Utb6bwQ&usqp=CAU" alt="sign up"/>
+      <form action="null">
+       
+        <div>
+          EMAIL ID:<input type="email" name="MYEMAIL"/>
+        </div>
+        <div>
+          Password:<input type="password" name="MYPASSWORD"/>
+        </div>
+        <div className="submit">
+          <input type="Submit" value="Login As A Teacher"/>
+        </div>
+        <div className="submit">
+          <input type="Submit" value="Login As A Student"/>
+        </div>
+        <div >
+          <input type="reset" value="RESET"/>
+          <input  type="Submit" value="Forget Password ?"/>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- flashy-fries/src/components/Signup/Signup.js
+++ flashy-fries/src/components/Signup/Signup.js
@@ -1,5 +1,30 @@
+import "./Signup.css";
 const Signup = () => {
-  return "signup";
+  return (
+    <div className="form">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC1ueC3iCmaW5la1MI4pF2AY2Yp20Utb6bwQ&usqp=CAU" alt="sign up"/>
+      <form action="null">
+        <div>
+          Name:<input type="text" name="MYNAME"/>
+        </div>
+        <div>
+          EMAIL ID:<input type="email" name="MYEMAIL"/>
+        </div>
+        <div>
+          Password:<input type="password" name="MYPASSWORD"/>
+        </div>
+        <div className="submit">
+          <input type="Submit" value="Submit As A Teacher"/>
+        </div>
+        <div className="submit">
+          <input type="Submit" value="Submit As A Student"/>
+        </div>
+        <div >
+          <input type="reset" value="RESET"/>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
