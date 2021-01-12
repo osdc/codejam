65380af94c8cf1987c9d26629904c45d67abfcfd
akshat jain <akshat.gms@gmail.com>
2021-01-12 23:51:52 +0530

    dashboard added

--- flashy-fries/src/components/Dashboard/Dashboard.js
+++ flashy-fries/src/components/Dashboard/Dashboard.js
@@ -1,5 +1,41 @@
+import "./Dashboard.css";
 function Dashboard() {
-  return "Dashboard";
+  return(
+    <div>
+      <h1 className="heading">WELCOME TO STUDY DATE</h1>
+      <div>
+        <h1 className="heading1">LIST OF TOPICS</h1>
+      </div>
+      <div className="select">
+      <label class="container">One
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+</label>
+      </div>
+      <div>
+      <label class="container">Two
+  <input type="checkbox"/>
+  <span class="checkmark"></span>
+</label>
+      </div>
+      <div>
+      <label class="container">Three
+  <input type="checkbox"/>
+  <span class="checkmark"></span>
+</label>
+      </div>
+      <div>
+      <label class="container">Four
+  <input type="checkbox"/>
+  <span class="checkmark"></span>
+</label>
+
+      </div>
+      <div>
+        <button className="btn">Submit</button>
+      </div>
+      </div> 
+  );
 }
 
 export default Dashboard;
