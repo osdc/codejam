91b18f85a988cb01c770965fbb9ac533a5900f7f
osBins <arpitjain.2001@gmail.com>
2021-01-13 16:02:29 +0530

    added homepage descriptions

--- flashy-fries/src/components/Homepage/Homepage.js
+++ flashy-fries/src/components/Homepage/Homepage.js
@@ -17,7 +17,15 @@ function Homepage() {
         <div className="descr">
           Studying | Dating (ye isi font mein)<br />
           Two far ends of the spectrum, right? (this in some other sans serif font)
-          Get the best of both worlds with Study Date™
+          Get the best of both worlds with Study Date™ (sans serif) <br />
+          We combine studying and online dating, to help you score in all spheres of life!
+          <br />
+          How to use (as a heading) <br />
+          * Visit the Study Date™ website and create an account on it for free. You are given the option to register either as a **student** or as a **teacher**. <br />
+* Choose the topic you want to study with or teach a random online date.<br />
+* You can either choose to study with a student (Study Date™) or a teacher.<br />
+* Our backend matches you with someone who wants to study the same topic and initiates a video call for you with them.<br />
+* After you are done studying on video call, you can obtain your reward (cute cat photos) for completing the Study Date™.<br />
           <br />
           ......
         </div>
