defc6595ffbb4454151c7cb508f93713bafaa70f
Aayush Kumar Sahu <54525741+aayushmau5@users.noreply.github.com>
2021-01-14 19:13:16 +0530

    Add randomly generatable jitsi meet link functionality (#7)
    * Add randomly generatable jitsi meet link
    
    * Redirect to jisti added
    
    * minor change

--- flashy-fries/src/components/Dashboard/Dashboard.js
+++ flashy-fries/src/components/Dashboard/Dashboard.js
@@ -1,21 +1,28 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { nanoid } from "nanoid";
 
 import { useAuth } from "../AuthContext";
 import { db } from "../firebase";
 import "./Dashboard.css";
+
+let newMeeting = true;
 
 function Dashboard() {
   const [topics, setTopics] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [docId, setDocId] = useState("");
   const [error, setError] = useState("");
+  const [meetLink, setMeetLink] = useState("");
+  const [meetingDocId, setMeetingDocId] = useState("");
+  const [membersCount, setMembersCount] = useState(0);
 
   const { register, handleSubmit, errors } = useForm();
 
   const { currentUser } = useAuth();
   const userEmail = currentUser.email;
   const userId = currentUser.uid;
+  const jitsiMeetHolder = "https://meet.jit.si/";
 
   function storeInDb(topics) {
     db.collection("studydatedata")
@@ -49,11 +56,61 @@ function Dashboard() {
       });
   }
 
+  function generateMeetLink() {
+    const meetId = nanoid();
+    const meetLink = jitsiMeetHolder + meetId;
+    return meetLink;
+  }
+
+  function checkIfTopicPresent(topicSelected) {
+    newMeeting = true;
+    db.collection("meetings")
+      .where("topic", "==", topicSelected)
+      .get()
+      .then((result) => {
+        result.forEach((val) => {
+          if (val.data().members !== 2) {
+            console.log("inside function running");
+            setMeetLink(val.data().meetlink);
+            newMeeting = false;
+            setMeetingDocId(val.data().id);
+            setMembersCount(val.data().members);
+          }
+        });
+        if (newMeeting) {
+          const link = generateMeetLink();
+          console.log("new meeting link generated");
+          return db
+            .collection("meetings")
+            .add({
+              topic: topicSelected,
+              meetlink: link,
+              members: 0,
+            })
+            .then((doc) => {
+              setMeetLink(link);
+              setMeetingDocId(doc.id);
+            });
+        }
+      })
+      .catch((err) => {
+        setError("Error setting meeting: " + err);
+      });
+  }
+
   function changeTopics(topics) {
     db.collection("studydatedata").doc(docId).update({
       topics: topics,
     });
     setIsEditing(false);
+  }
+
+  function updateMeetingStatus() {
+    db.collection("meetings")
+      .doc(meetingDocId)
+      .update({
+        members: membersCount + 1,
+      });
   }
 
   function onSubmit(data) {
@@ -72,8 +129,20 @@ function Dashboard() {
       <h1 className="heading">WELCOME TO STUDY DATE</h1>
       <h2 className="heading3">&nbsp;&nbsp;&nbsp;Your Email: {userEmail}</h2>
       <div>
-        <h1 className="heading1">&nbsp;&nbsp;List of Topics you have selected</h1>
+        <h1 className="heading1">
+          &nbsp;&nbsp;List of Topics you have selected
+        </h1>
       </div>
+      <h3>
+        {meetLink !== "" ? (
+          <>
+            {`Join meeting: ${meetLink}`}
+            <a href={meetLink} className="btn" onClick={updateMeetingStatus}>
+              Join
+            </a>
+          </>
+        ) : null}
+      </h3>
       {isEditing ? (
         <>
           <form onSubmit={handleSubmit(onSubmit)}>
@@ -135,7 +204,17 @@ function Dashboard() {
       ) : (
         <ul>
           {topics.map((topic) => {
-            return <li className="heading3" key={topic}>{topic}</li>;
+            return (
+              <li key={topic}>
+                {topic}{" "}
+                <button
+                  className="btn"
+                  onClick={() => checkIfTopicPresent(topic)}
+                >
+                  Meet
+                </button>
+              </li>
+            );
           })}
         </ul>
       )}
