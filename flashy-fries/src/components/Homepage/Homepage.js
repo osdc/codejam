5f0475be26f97bd11817f8c39756ef882abab3f6
osBins <arpitjain.2001@gmail.com>
2021-01-14 13:14:52 +0530

    fixed positioning of homepage elements (2)

--- flashy-fries/src/components/Homepage/Homepage.js
+++ flashy-fries/src/components/Homepage/Homepage.js
@@ -20,12 +20,12 @@ function Homepage() {
       <div className="descr">
          <div className="head2"><br /> &nbsp;&nbsp;Studying | Dating<br /></div>
           <div className="first">
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Two far ends of the spectrum, right?</div>
-          <div className="second"> Get the best of both worlds with Study Date <br /></div>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We combine studying with online dating to offer a unique experience!
-          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Two far ends of the spectrum, right?<br /></div>
+          <div className="second">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Get the best of both worlds with Study Date <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We combine studying with online dating to offer a unique experience!<br />
+          <br /></div>
          <h2 className="third"> &nbsp;&nbsp;How to use<br /></h2>
-          <ul> <li> Visit the Study Date website and create an account on it for free. You are given the option to register either as a student or as a teacher. <br />
+          <ul> <li> Visit the Study Date website and create an account on it for free.<br /> You are given the option to register either as a student or as a teacher. <br />
 </li>
 <li>Choose the topic you want to study with or teach a random online date.<br />
 </li>
@@ -33,10 +33,10 @@ function Homepage() {
  You can either choose to study with a student (Study Date) or a teacher.<br />
  </li>
  <li>
-Our backend matches you with someone who wants to study the same topic and initiates a video call for you with them.<br />
+Our backend matches you with someone who wants to study the same <br />topic and initiates a video call for you with them.<br />
 </li>
 <li>
-After you are done studying on video call, you can obtain your reward (cute dog & cat photos) for completing the Study Date.<br />
+After you are done studying on video call, you can obtain your reward <br />(cute dog & cat photos) for completing the Study Date.<br />
     </li>
         </ul>
         </div>
